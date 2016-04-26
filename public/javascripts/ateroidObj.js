5e2bbecf9aab714b2792962aeabef328ae7b9002
Nick MacIntyre <nickmac@Nick-MacBook-Air.local>
2016-04-26 10:51:12 -0600

    adding roids..

---
+++ public/javascripts/ateroidObj.js
@@ -0,0 +1,64 @@
+
+function Asteroids (width, height, size, pic, ship, bullets, breaker){
+  this.x = 0;
+  this.y = 0;
+  this.vx = 0;
+  this.vy = 0;
+  this.size= size;
+  this.img= pic;
+  this.val = true;
+  this.width = width;
+  this.height = height;
+  this.start = function (px, py) {
+    var rx = Math.random() * (3 + 3) - 3;
+    var ry = Math.random() * (3 + 3) - 3;
+    this.vy = rx != 0 ? rx : 1;
+    this.vx = ry != 0 ? ry : -1;
+    if(px){
+      this.x = px;
+      this.y = py;
+    }else{
+      var top = (Math.random() * (507 - 0));
+      var bot = (Math.random() * (1067.25 - 0));
+      this.x = 200 * ( 1 - ( top/bot) ) + 50;
+      this.y = 200 * ( 1 - ( bot/top )) + 50;
+    }
+  };
+  this.draw = function () {
+    this.x += this.vx;
+    this.y += this.vy;
+    // this.bounce();
+    this.wall();
+  }
+  this.bounce = function () {
+    for (var i = 0; i < ships.length; i++) {
+      if (this.x <= ships[i].x  &&
+        this.x + this.width  > ships[i].x &&
+        this.y < ships[i].y  &&
+        this.height + this.y > ships[i].y) {
+          ships[i].val = false;
+    }
+  }
+}
+    // for (var i = 0; i < bullets.length; i++) {
+    //   if (this.x < bullets[i].x  &&
+    //     this.x + this.width  > bullets[i].x &&
+    //    this.y < bullets[i].y  &&
+    //    this.height - 30 + this.y > bullets[i].y && bullets[i].val === true) {
+    //      bullets[i].val = false;
+    //      this.val = false;
+    //      if( this.size === 'big'){
+    //        breaker (this.x, this.y, 'big');
+    //      }if(this.size === 'small'){
+    //        breaker (this.x, this.y, 'small');
+    //      }
+    //   }
+    // }
+  this.wall = function () {
+    if(this.x > 1067.25){this.x = 0 - this.width/2};
+    if(this.x + this.width < 0){this.x = 1067.25};
+    if(this.y > 507){this.y = 0 - this.height/2};
+    if(this.y + this.height < 0){this.y = 507};
+  }
+}
+module.exports = Asteroids;
---
+++ public/javascripts/clientGame.js
@@ -0,0 +1,78 @@
+var socket = io();
+var canvas = document.getElementById("a");
+var ctx = canvas.getContext("2d");
+var player;
+ctx.canvas.width = 1067.25;
+ctx.canvas.height = 507;
+var shipimg = [];
+shipimg[1] = new Image();
+  shipimg[1].src = '/images/mship1.png';
+shipimg[0] = new Image();
+  shipimg[0].src = '/images/flacon.png';
+// var astroidMed = new Image();
+//     astroidMed.src = 'pic/Astromedium.png'
+// var astroidBig = new Image();
+//   astroidBig.src = 'pic/Asteroid.png'
+// var astroidtinny = new Image();
+//   astroidtinny.src = 'pic/tinnya.png'
+
+
+var keysDown = {};
+addEventListener("keydown", function (e) {
+  keysDown[e.keyCode] = true;
+}, false);
+addEventListener("keyup", function (e) {
+  delete keysDown[e.keyCode];
+}, false);
+
+socket.on('self', function(playernum){
+  player = playernum;
+})
+
+function move (ship) {
+  console.log(ship);
+  if(39 in keysDown ) {ship.rad += 6*(Math.PI/180)};
+  if(37 in keysDown) {ship.rad -= 6*(Math.PI/180)};
+  if(ship.rad === 360*(Math.PI/180) || ship.rad === -360*(Math.PI/180)){
+    ship.rad = 0;
+  }
+  if(38 in keysDown){
+    ship.vx += ship.thrust * Math.sin(ship.rad);
+    ship.vy += ship.thrust * Math.cos(ship.rad);
+  }if(40 in keysDown){
+    ship.vx -= ship.thrust * Math.sin(ship.rad);
+    ship.vy -= ship.thrust * Math.cos(ship.rad);
+  }
+}
+
+function shipsDraw (ships){
+  for (var i = 0; i < ships.length; i++) {
+    if (ships[i]) {
+      ships[i].img = shipimg[i];
+      ctx.save();
+      ctx.translate(ships[i].x, ships[i].y);
+      ctx.rotate(ships[i].rad);
+      ctx.drawImage(shipimg[i], -(ships[i].img.width/2), -(ships[i].img.height/2));
+      ctx.restore();
+      ctx.save();
+    }
+  }
+}
+function roidsDraw (roids) {
+  for (var i = 0; i < roids.length; i++) {
+    ctx.drawImage(shipimg[0], roids[i].x , roids[i].y )
+  }
+}
+
+socket.on('png', function(obj){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  move(obj.ships[player]);
+  shipsDraw(obj.ships)
+  roidsDraw(obj.roids)
+  socket.emit('update', {ship: obj.ships[player], player: player})
+})
+
+window.setInterval(function () {
+  socket.emit('cat');
+}, 10);
